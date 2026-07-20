987bb81b70651ad19cb8409bcb0ad3f9eb1e75ff
u2404057-cuet <u2404057@student.cuet.ac.bd>
2026-07-20 19:44:59 +0600

    chore(server): set BETTER_AUTH_URL as baseURL and CLIENT_URL in trustedOrigins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,10 +6,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:8000",
   database: mongodbAdapter(db, {
     client: client
   }),
-  trustedOrigins: [process.env.CLIENT_URL || "http://localhost:3000"],
+  trustedOrigins: [
+    process.env.CLIENT_URL || "http://localhost:3000",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://jobspark-zeta.vercel.app"
+  ],
   emailAndPassword: {
     enabled: true,
   },
